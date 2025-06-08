98c8d36125efbd8ae750e3512ac71504a1f91760
Ahmed <ahmed@survey-awesome.com>
2025-06-08 06:05:05 +0530

    code changes

--- get-images.js
+++ get-images.js
@@ -3,7 +3,7 @@ import * as fs from "fs";
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const writeImges = async function (imageURLArr, catName, index) {
+const writeImges = async function ({ images: imageURLArr, category: catName, index }) {
 	for (let i = 0; i < imageURLArr.length; i++) {
 		const imageURL = imageURLArr[i];
 		const splits = imageURL.split(".");
@@ -22,37 +22,23 @@ const writeImges = async function (imageURLArr, catName, index) {
 				console.log(`Image ${imageName} saved successfully.`);
 			} catch (error) {
 				console.error(`Error fetching image ${imageName}:`, err);
-				errorImges.push(imageName);
-				fs.writeFileSync("./errorImages.txt", errorImges.join("\n"), { encoding: "utf-8" });
+				errorImges.push({ images: imageURLArr, category: catName, index });
+				fs.writeFileSync("./errorImages.txt", JSON.stringify(errorImges), { encoding: "utf-8" });
 			}
+			await sleep(500 + Math.floor(Math.random() * 500));
 		}
 	}
 };
 
-const res = fs.readdirSync("./").filter((fileName) => fileName.endsWith(".json"));
+const res = fs.readFileSync("./mergedImages.json", { encoding: "utf-8" });
 
-const jsonFiles = res
-	.map((fileName) => {
-		const fileContent = fs.readFileSync(fileName, { encoding: "utf-8" });
-		const fileJSON = JSON.parse(fileContent);
-
-		return fileJSON;
-	})
-	.flat();
-
-const mergedJSON = jsonFiles.map((rawFiles) => {
-	const refFileds = {
-		images: rawFiles.image,
-		category: rawFiles.category,
-		index: rawFiles.index
-	};
-	return refFileds;
-});
+const mergedJSON = JSON.parse(res);
 
 const errorImges = [];
+
 for (let i = 0; i < mergedJSON.length; i++) {
-	const { images, category, index } = mergedJSON[i];
-	await writeImges(images, category, index);
-	await sleep(500 + Math.floor(Math.random() * 500)); // Sleep for 0.5 to 1 second
+	const thisObject = mergedJSON[i];
+	await writeImges(thisObject);
+
 	console.log(`Processed ${i + 1}/${mergedJSON.length} images.`);
 }
--- working.js
+++ working.js
@@ -1,19 +1,60 @@
 import * as fs from "fs";
 
-const filePath = `./images_Home_and_Kitchen_5.json`;
-const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });
-const fileJSON = JSON.parse(fileContent);
+// const filePath = `./images_Home_and_Kitchen_5.json`;
+// const fileContent = fs.readFileSync(filePath, { encoding: "utf-8" });
+// const fileJSON = JSON.parse(fileContent);
 
-const chunks = function (array, size) {
-	const results = [];
-	while (array.length) {
-		results.push(array.splice(0, size));
-	}
-	return results;
+// const chunks = function (array, size) {
+// 	const results = [];
+// 	while (array.length) {
+// 		results.push(array.splice(0, size));
+// 	}
+// 	return results;
+// };
+// const fileChunks = chunks(fileJSON, Math.ceil(fileJSON.length / 5));
+
+// fileChunks.forEach((chunk, index) => {
+// 	fs.writeFileSync(`./images_Home_and_Kitchen_${index + 1}_5.json`, JSON.stringify(chunk), { encoding: "utf-8" });
+// 	console.log(`Chunk ${index + 1} written successfully.`);
+// });
+// const res = fs.readdirSync("./").filter((fileName) => fileName.endsWith(".json") && !fileName.startsWith("merged"));
+// console.log(res);
+
+// const jsonFiles = res
+// 	.map((fileName) => {
+// 		const fileContent = fs.readFileSync(fileName, { encoding: "utf-8" });
+// 		const fileJSON = JSON.parse(fileContent);
+
+// 		return fileJSON;
+// 	})
+// 	.flat();
+
+// const mergedJSON = jsonFiles.map((rawFiles) => {
+// 	const refFileds = {
+// 		images: rawFiles.image,
+// 		category: rawFiles.category,
+// 		index: rawFiles.index
+// 	};
+// 	return refFileds;
+// });
+
+// // fs.writeFileSync("./mergedImages.json", JSON.stringify(mergedJSON), { encoding: "utf-8" });
+
+// console.log("Merged JSON file created successfully with", mergedJSON.length, "entries.");
+
+// const mf = fs.readFileSync("./mergedImages.json", { encoding: "utf-8" });
+// const mfJSON = JSON.parse(mf);
+
+// console.log("Merged JSON file has", mfJSON.length, "entries.");
+
+const writeImges = async function ({ images: imageURLArr, category: catName, index }) {
+	console.log({ imageURLArr, catName, index });
 };
-const fileChunks = chunks(fileJSON, Math.ceil(fileJSON.length / 5));
 
-fileChunks.forEach((chunk, index) => {
-	fs.writeFileSync(`./images_Home_and_Kitchen_${index + 1}_5.json`, JSON.stringify(chunk), { encoding: "utf-8" });
-	console.log(`Chunk ${index + 1} written successfully.`);
-});
+const res = fs.readFileSync("./mergedImages.json", { encoding: "utf-8" });
+
+const mergedJSON = JSON.parse(res);
+for (let i = 0; i < 10; i++) {
+	const thisObject = mergedJSON[i];
+	await writeImges(thisObject);
+}
